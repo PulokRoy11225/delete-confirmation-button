0e18d39d2c484b25fb453508c1e9108106340761
PulokRoy11225 <pulokr221@gmail.com>
2023-10-12 22:33:40 +0800

    fist commit

---
+++ js/events.js
@@ -0,0 +1,13 @@
+console.log('This is separate JS file');
+
+
+// option 1
+// directly set on the HTML element
+/**option 2---mostly used
+ * with onclick function on the html element
+ * option 3
+ * with id
+ * 
+ * option 4
+ * last option will be used the most
+ */
